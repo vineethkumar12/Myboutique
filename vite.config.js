19ce7dff3bb62979c982a5c033cb24599de4856b
vineethkumar12 <adepuvineethkumarvinni@gmail.com>
2025-02-23 17:18:22 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     host: "localhost", // Strictly use localhost
     port: 5173,
